4b703dfab0c2d14f5247708dbca19c1d398803ee
z502wa <z502.wa@gmail.com>
2025-02-19 09:36:59 +0000

    holberton

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,20 +1,23 @@
 #include "main.h"
+
 /**
  * main - Entry point
  *
- * Description: Prints "_putchar" followed by a new line
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	char text[] = "_putchar";
-	int i;
+	char str[] = "_putchar\n";
 
-	for (i = 0; text[i] != '\0'; i++)
+	int i = 0;
+
+	while (str[i])
 	{
-		_putchar(text[i]);
+		_putchar(str[i]);
+
+		i++;
 	}
-	_putchar('\n');
 
 	return (0);
 }
