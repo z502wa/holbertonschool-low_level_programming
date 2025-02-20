f09e997f346d7822becd854826d0d12d5d83ad09
root <z502.wa@gmail.com>
2025-02-20 14:10:31 +0000

    holberton

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -8,8 +8,8 @@
  */
 int _islower(int c)
 {
-    if (c >= 'a' && c <= 'z') /* استخدم المسافات بدل التاب */
-        return (1);
-    else
-        return (0);
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
 }
