7c646cc786cb4874aec978aacff270a3ad27bc01
z502wa <z502.wa@gmail.com>
2025-02-20 21:13:09 +0000

    holberton

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,6 +1,30 @@
 #include "main.h"
 
 /**
+ * print_number - Prints a number using _putchar
+ * @n: The number to print
+ */
+void print_number(int n)
+{
+	if (n >= 100)
+	{
+		_putchar((n / 100) + '0');
+		_putchar(((n / 10) % 10) + '0');
+	}
+	else if (n >= 10)
+	{
+		_putchar(' ');
+		_putchar((n / 10) + '0');
+	}
+	else
+	{
+		_putchar(' ');
+		_putchar(' ');
+	}
+	_putchar((n % 10) + '0');
+}
+
+/**
  * print_times_table - Prints the n times table, starting with 0
  * @n: The number defining the times table range
  */
@@ -8,45 +32,24 @@ void print_times_table(int n)
 {
 	int i, j, product;
 
-	if (n >= 0 && n <= 15)
+	if (n < 0 || n > 15)
+		return;
+
+	for (i = 0; i <= n; i++)
 	{
-		for (i = 0; i <= n; i++)
+		for (j = 0; j <= n; j++)
 		{
-			for (j = 0; j <= n; j++)
+			product = i * j;
+
+			if (j == 0)
+				_putchar(product + '0');
+			else
 			{
-				product = i * j;
-
-				if (j == 0)
-				{
-					_putchar(product + '0');  /* طباعة الرقم الأول بدون مسافات */
-				}
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-
-					/* تنسيق الأرقام بحيث تظهر بشكل صحيح */
-					if (product < 10)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(product + '0');
-					}
-					else if (product < 100)
-					{
-						_putchar(' ');
-						_putchar((product / 10) + '0');
-						_putchar((product % 10) + '0');
-					}
-					else
-					{
-						_putchar((product / 100) + '0');
-						_putchar(((product / 10) % 10) + '0');
-						_putchar((product % 10) + '0');
-					}
-				}
+				_putchar(',');
+				_putchar(' ');
+				print_number(product);
 			}
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
