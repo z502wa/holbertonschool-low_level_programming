35004fae71a1e3e707d0cb673380744514fd47e6
z502wa <z502.wa@gmail.com>
2025-02-24 15:29:13 +0000

    holberton

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Tests the mul function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%d\n", mul(98, 1024));
+	printf("%d\n", mul(-402, 4096));
+	return (0);
+}
