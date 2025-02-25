6aa54082e9c60994e1241c40718b63ec69d63205
z502wa <z502.wa@gmail.com>
2025-02-25 13:23:12 +0000

    holberton

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints numbers from 0 to 14, ten times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i, num;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -9,4 +9,6 @@ void print_numbers(void); /* Function prototype for print_numbers */
 int _putchar(char c); /* Function prototype for _putchar */
 void print_most_numbers(void);
 int _putchar(char c);
+void more_numbers(void); /* Function prototype for printing numbers */
+int _putchar(char c); /* Function prototype for _putchar */
 #endif
