5ea3fb9a6c9e5d480c9a1e7722f6882004e2dfd1
z502wa <z502.wa@gmail.com>
2025-02-25 12:38:48 +0000

    holberton

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -7,4 +7,6 @@ int mul(int a, int b); /* Function prototype for multiplication */
 void print_number(int n); /* Prototype for number printing function */
 void print_numbers(void); /* Function prototype for print_numbers */
 int _putchar(char c); /* Function prototype for _putchar */
+void print_most_numbers(void);
+int _putchar(char c);
 #endif
