a172e29d2c556eb9fb55323cebfb09ed392f567b
z502wa <z502.wa@gmail.com>
2025-02-27 17:25:33 +0000

    holberton

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -11,4 +11,9 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 #endif /* MAIN_H */
