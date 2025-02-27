afda29885282ecb80ce1981315cd80b4f5899289
z502wa <z502.wa@gmail.com>
2025-02-27 19:34:24 +0000

    hol

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
+
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -30,5 +31,5 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-
+char *rot13(char *);/**task 8*/
 #endif /* MAIN_H */
