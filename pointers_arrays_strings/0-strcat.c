388df9a60a2166a787e61dacf11e6d81c6013aa5
z502wa <z502.wa@gmail.com>
2025-02-27 19:12:51 +0000

    holberton

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+*_strcat - Concatenates two strings.
+*@dest: The destination string.
+*@src: The source string.
+*
+*Return: Pointer to the resulting string dest.
+*/
+char *_strcat(char *dest, char *src)
+{
+int i = 0, j = 0;
+
+while (dest[i] != '\0')
+{
+i++;
+}
+
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+
+dest[i] = '\0';
+
+{return dest; }
+}
