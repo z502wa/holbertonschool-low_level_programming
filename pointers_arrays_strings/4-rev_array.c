c91d39219c63cd5226777c8ac0aff8becb03fed1
z502wa <z502.wa@gmail.com>
2025-02-27 19:22:10 +0000

    holberton

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * reverse_array - Reverses the content of an array of integers
+ * @a: Array of integers
+ * @n: Number of elements in the array
+ */
+void reverse_array(int *a, int n)
+{
+int i, temp;
+
+for (i = 0; i < n / 2; i++)
+{
+temp = a[i];
+a[i] = a[n - 1 - i];
+a[n - 1 - i] = temp;
+}
+}
