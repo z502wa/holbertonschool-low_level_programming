460323d58813483bc7b9677e5ad6b448de071023
z502wa <z502.wa@gmail.com>
2025-02-27 18:25:32 +0000

    holberton

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -3,26 +3,30 @@
 #include <time.h>
 
 /**
- * main - Generates a random valid password for the program 101-crackme.
+ * main - Generates a random valid password for `101-crackme`.
  *
  * Return: Always 0.
  */
 int main(void)
 {
 	int sum = 0, n;
+	int i = 0; /* تم تعريف i خارج الحلقة */
 	char password[100];
 
 	srand(time(0));
 
-	for (int i = 0; sum < 2772 - 122; i++)
+	/* توليد أحرف عشوائية بحيث يكون مجموعها 2772 */
+	while (sum < 2772 - 122)
 	{
-		n = rand() % 94 + 33; /* Generate printable ASCII characters */
+		n = rand() % 94 + 33; /* توليد حرف ASCII قابل للطباعة */
 		password[i] = n;
 		sum += n;
+		i++;
 	}
 
-	password[sum - (2772 - 122)] = (2772 - sum);
-	password[sum - (2772 - 122) + 1] = '\0';
+	/* التأكد من أن المجموع النهائي هو 2772 */
+	password[i] = (2772 - sum);
+	password[i + 1] = '\0';
 
 	printf("%s", password);
 	return (0);
