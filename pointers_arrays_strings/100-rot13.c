7b5fd502bb3dc2eeacbbc8240ff31aae4e6a2fa7
z502wa <z502.wa@gmail.com>
2025-02-27 19:47:02 +0000

    holberton

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -1,18 +1,31 @@
 #include "main.h"
-#include <stdio.h>
 
+/**
+ * rot13 - Encodes a string using ROT13 cipher.
+ * @s: Pointer to the input string.
+ *
+ * Description: Each letter is replaced with the letter 13 positions
+ * after it in the alphabet. Non-alphabetic characters remain unchanged.
+ *
+ * Return: Pointer to the encoded string.
+ */
 char *rot13(char *s)
 {
-	int i = 0;
+	int i, j;
+	char *alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	while (s[i] != '0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		printf ("%c" , *s);
-
+		for (j = 0; alphabet[j] != '\0'; j++)
+		{
+			if (s[i] == alphabet[j])
+			{
+				s[i] = rot13[j];
+				break;
+			}
+		}
 	}
-	
-	i++;
+
 	return (s);
-
-
 }
