26e145579ee86280068d047068ca00f801b32733
z502wa <z502.wa@gmail.com>
2025-03-07 18:23:20 +0000

    holberton

--- recursion/main.h
+++ recursion/main.h
@@ -7,7 +7,7 @@ void _print_rev_recursion(char *s);
 int _putchar(char c); /* Prototype for _putchar function */
 int _strlen_recursion(char *s);
 int factorial(int n);
-
+int _pow_recursion(int x, int y);
 
 
 
