8f58c57eddfc1b5c45b8184125905c7058072700
z502wa <z502.wa@gmail.com>
2025-03-14 19:52:17 +0000

    holberton

---
+++ malloc_free/100-argstostr.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - Concatenates all arguments of the program.
+ * @ac: The argument count.
+ * @av: The argument vector.
+ *
+ * Return: Pointer to new string containing all arguments separated by \n,
+ * or NULL on failure.
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int i, j, k = 0, len = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			len++;
+		len++; /* For newline */
+	}
+
+	str = malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			str[k++] = av[i][j];
+		str[k++] = '\n';
+	}
+	str[k] = '\0';
+
+	return (str);
+}
