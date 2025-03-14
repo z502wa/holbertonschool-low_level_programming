7327e7e49653dc02539722ade909a9cb9034f31d
z502wa <z502.wa@gmail.com>
2025-03-14 20:39:18 +0000

    holberton

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - Allocates memory for an array and initializes it to zero.
+ * @nmemb: Number of elements in the array.
+ * @size: Size of each element in bytes.
+ *
+ * Return: Pointer to the allocated memory, or NULL if failure occurs.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		ptr[i] = 0;
+
+	return (ptr);
+}
--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -2,6 +2,9 @@
 #define MAIN_H
 
 void *malloc_checked(unsigned int b);
-int _putchar(char c);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+int _putchar(char c);
+
 #endif /* MAIN_H */
