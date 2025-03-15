7dd6eba45c8b2c48025a77cf59c075a2b9af23ab
z502wa <z502.wa@gmail.com>
2025-03-15 20:09:30 +0000

    holberton

---
+++ structures_typedef/dog.h
@@ -0,0 +1,29 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure for a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: This structure defines the properties of a dog,
+ * including its name, age, and owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * init_dog - Initializes a struct dog
+ * @d: Pointer to struct dog to initialize
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif /* DOG_H */
