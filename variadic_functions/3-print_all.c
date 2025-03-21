533d2348e3254890b52d2eb68f22b93c84f13061
z502wa <z502.wa@gmail.com>
2025-03-21 19:32:44 +0000

    holberton

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,44 +1,84 @@
+/*
+ * File: 3-print_all.c
+ * Author: Your Name
+ * Description: Function that prints anything based on format specifiers.
+ */
+
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
 
 /**
- * print_all - prints anything based on the format
- * @format: list of types of arguments passed to the function
- *
- * Return: void
+ * print_char - Prints a character
+ * @args: Argument list
+ */
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+
+/**
+ * print_int - Prints an integer
+ * @args: Argument list
+ */
+void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+
+/**
+ * print_float - Prints a float
+ * @args: Argument list
+ */
+void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+
+/**
+ * print_string - Prints a string
+ * @args: Argument list
+ */
+void print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+
+	if (!str)
+		str = "(nil)";
+	printf("%s", str);
+}
+
+/**
+ * print_all - Prints anything based on the format
+ * @format: List of types of arguments passed
  */
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	unsigned int i = 0;
-	char *str, *sep = "";
+	unsigned int i = 0, j;
+	char *separator = "";
+	fmt_t fmt_list[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+		{0, NULL}
+	};
 
 	va_start(args, format);
 	while (format && format[i])
 	{
-		if (format[i] == 'c')
+		j = 0;
+		while (fmt_list[j].type)
 		{
-			printf("%s%c", sep, va_arg(args, int));
-			sep = ", ";
-		}
-		if (format[i] == 'i')
-		{
-			printf("%s%d", sep, va_arg(args, int));
-			sep = ", ";
-		}
-		if (format[i] == 'f')
-		{
-			printf("%s%f", sep, va_arg(args, double));
-			sep = ", ";
-		}
-		if (format[i] == 's')
-		{
-			str = va_arg(args, char *);
-			if (str == NULL)
-				str = "(nil)";
-			printf("%s%s", sep, str);
-			sep = ", ";
+			if (format[i] == fmt_list[j].type)
+			{
+				printf("%s", separator);
+				fmt_list[j].f(args);
+				separator = ", ";
+				break;
+			}
+			j++;
 		}
 		i++;
 	}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -3,12 +3,21 @@
 
 #include <stdarg.h>
 
+/**
+ * struct fmt_t - Struct for format types
+ * @type: The format specifier
+ * @f: The function associated
+ */
+typedef struct fmt_t
+{
+	char type;
+	void (*f)(va_list);
+} fmt_t;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-
 
 #endif /* VARIADIC_FUNCTIONS_H */
