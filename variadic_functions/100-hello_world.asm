61dee2307a7ff1bb052cae07c74eefa18ff28ae9
z502wa <z502.wa@gmail.com>
2025-03-21 20:00:40 +0000

    holberton

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,27 +1,14 @@
-; File: 100-hello_world.asm
-; Assemble with:
-;   nasm -f elf64 100-hello_world.asm
-; Link with:
-;   ld -o hello 100-hello_world.o
-; أو كما ذُكر في التعليمات:
-;   nasm -f elf64 100-hello_world.asm && gcc -no-pie -std=gnu89 100-hello_world.o -o hello
-
-global _start
-
-section .data
-    msg db "Hello, World", 10   ; النص المراد طباعته + سطر جديد
-    len equ $ - msg             ; حساب طول النص
-
 section .text
-_start:
-    ; استدعاء syscall للكتابة
-    mov rax, 1                  ; رقم استدعاء النظام للكتابة (write)
-    mov rdi, 1                  ; الرقم المميز لـ stdout
-    mov rsi, msg                ; عنوان بداية الرسالة
-    mov rdx, len                ; طول الرسالة
-    syscall
+	  global main
+main:
+	    mov rax, 1
+	    mov rdi, 1
+	    mov rsi, message
+	    mov rdx, 17
+	    syscall
 
-    ; استدعاء syscall للخروج
-    mov rax, 60                 ; رقم استدعاء النظام للخروج (exit)
-    xor rdi, rdi                ; كود الخروج 0
-    syscall
+	    mov rax, 60
+	    xor rdi, rdi
+	    syscall
+	  section .data
+message:	 db "Hello, Holberton", 0xA, 0x0
