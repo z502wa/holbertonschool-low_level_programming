413ae2d11413df30e9f25ea87fdea21831af8752
z502wa <z502.wa@gmail.com>
2025-04-02 19:33:59 +0000

    holberton

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - Frees a list_t list
+ * @head: Pointer to the head of the list
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
