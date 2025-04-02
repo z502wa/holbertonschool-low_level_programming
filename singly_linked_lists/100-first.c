f252a32bbe244c37a6426acabd2985f6d06d6263
z502wa <z502.wa@gmail.com>
2025-04-02 19:46:43 +0000

    holberton

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * first - Prints a sentence before main is executed
+ *
+ * Return: nothing
+ */
+void __attribute__((constructor)) first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
