46b3abf545760b5de82c7a713412f6b3d3fbc8db
z502wa <z50.wa@gmail.com>
2025-04-05 07:49:22 +0000

    holberton

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,85 +1,25 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: string containing the binary number
+ * flip_bits - returns the number of bits needed
+ * to flip from one number to another
+ * @n: the first number
+ * @m: the second number
  *
- * Return: the converted number, or 0 if b is NULL or contains invalid chars
+ * Return: number of bits that must be flipped
  */
-unsigned int binary_to_uint(const char *b)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int result = 0;
-	int i = 0;
+	unsigned long int xor_val = n ^ m;
+	unsigned int count = 0;
 
-	if (b == NULL)
-		return (0);
-
-	while (b[i] != '\0')
+	while (xor_val > 0)
 	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
-		result = result * 2 + (b[i] - '0');
-		i++;
+		/* Check the least significant bit */
+		count += (xor_val & 1);
+		/* Shift bits to the right by 1 */
+		xor_val >>= 1;
 	}
 
-	return (result);
-}
-
-/**
- * print_binary - prints the binary representation of a number
- * @n: number to print in binary
- */
-void print_binary(unsigned long int n)
-{
-	if (n >> 1)
-		print_binary(n >> 1);
-	_putchar((n & 1) + '0');
-}
-
-/**
- * get_bit - returns the value of a bit at a given index
- * @n: number to search
- * @index: index of the bit
- *
- * Return: value of the bit or -1 if error
- */
-int get_bit(unsigned long int n, unsigned int index)
-{
-	if (index >= sizeof(unsigned long int) * 8)
-		return (-1);
-
-	return ((n >> index) & 1);
-}
-
-/**
- * set_bit - sets the value of a bit to 1 at a given index
- * @n: pointer to the number
- * @index: index of the bit
- *
- * Return: 1 if successful, -1 if error
- */
-int set_bit(unsigned long int *n, unsigned int index)
-{
-	if (index >= sizeof(unsigned long int) * 8 || n == NULL)
-		return (-1);
-
-	*n |= (1UL << index);
-	return (1);
-}
-
-/**
- * clear_bit - sets the value of a bit to 0 at a given index
- * @n: pointer to the number
- * @index: index of the bit
- *
- * Return: 1 if successful, -1 if error
- */
-int clear_bit(unsigned long int *n, unsigned int index)
-{
-	if (index >= sizeof(unsigned long int) * 8 || n == NULL)
-		return (-1);
-
-	*n &= ~(1UL << index);
-	return (1);
+	return (count);
 }
