87040a5941b4f5a3cc7fec266cb3598b02f798a1
z502wa <z50.wa@gmail.com>
2025-04-05 07:32:43 +0000

    holberton

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,106 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number, or 0 if b is NULL or contains invalid chars
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int i = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		result = result * 2 + (b[i] - '0');
+		i++;
+	}
+
+	return (result);
+}
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number to print in binary
+ */
+void print_binary(unsigned long int n)
+{
+	if (n >> 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+}
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: number to search
+ * @index: index of the bit, starting from 0
+ *
+ * Return: value of the bit at index or -1 if error
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	return ((n >> index) & 1);
+}
+
+/**
+ * set_bit - sets the value of a bit to 1 at a given index
+ * @n: pointer to the number to modify
+ * @index: index of the bit to set
+ *
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8 || n == NULL)
+		return (-1);
+
+	*n |= (1UL << index);
+	return (1);
+}
+
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index
+ * @n: pointer to the number to modify
+ * @index: index of the bit to clear
+ *
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8 || n == NULL)
+		return (-1);
+
+	*n &= ~(1UL << index);
+	return (1);
+}
+
+/**
+ * flip_bits - returns the number of bits to flip to get from n to m
+ * @n: first number
+ * @m: second number
+ *
+ * Return: number of bits to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor = n ^ m;
+	unsigned int count = 0;
+
+	while (xor)
+	{
+		count += xor & 1;
+		xor >>= 1;
+	}
+
+	return (count);
+}
