7f9e91580ab311ee52568ac807b67df49564e310
z502wa <z50.wa@gmail.com>
2025-04-05 07:25:50 +0000

    holberton

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number, or 0 if b is NULL or contains invalid chars
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int i = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		result = result * 2 + (b[i] - '0');
+		i++;
+	}
+
+	return (result);
+}
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number to print in binary
+ */
+void print_binary(unsigned long int n)
+{
+	if (n >> 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+}
